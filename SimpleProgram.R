9d7c55c9af12c8fdae25a1ba749da8f61fd4b013
Myeongjong Kang <myeongjong@stat.tamu.edu>
2019-09-03 12:10:13 -0500

    Part 4: update the main file to save calculated MSE

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -17,5 +17,5 @@ Y <- generateY(X, beta, sigma)
 # [1] 1.775522 2.254873 1.929695 2.209177 2.335023 2.134707 2.104085 1.993899 1.877685 2.016066
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
-calculateMSE(beta, beta_LS)
+(MSE <- calculateMSE(beta, beta_LS))
 # 0.03205253
