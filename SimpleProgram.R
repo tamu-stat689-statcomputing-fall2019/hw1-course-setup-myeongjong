a8014720017b2b90439351bbf1d60cc08db7ecc9
Myeongjong Kang <myeongjong@stat.tamu.edu>
2019-09-03 12:33:04 -0500

    Optional: update the main file to compare two different versions of each function using microbenchmark

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,14 +8,22 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
+
 # [ToDo] Use generateY function to generate Y
 source("FunctionsLM.R")
-Y <- generateY(X, beta, sigma)
+Y <- generateY(X, beta, sigma) #  seed = 5832652
 
 # [ToDo] Use calculateBeta function to calculate beta_LS
 (beta_LS <- calculateBeta(X, Y))
-# [1] 1.775522 2.254873 1.929695 2.209177 2.335023 2.134707 2.104085 1.993899 1.877685 2.016066
+# Note: 1.775522 2.254873 1.929695 2.209177 2.335023 2.134707 2.104085 1.993899 1.877685 2.016066
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
 (MSE <- calculateMSE(beta, beta_LS))
-# 0.03205253
+# Note: 0.03205253
+
+
+# [Optional] microbenchmark
+require(microbenchmark)
+microbenchmark(calculateBeta_old(X, Y), calculateBeta(X, Y))
+microbenchmark(calculateMSE_old(beta, beta_LS), calculateMSE(beta, beta_LS))
+# Note: The new version is slightly faster than the old one.
